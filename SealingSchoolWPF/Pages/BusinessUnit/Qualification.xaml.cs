b99ab80df581bb9754bcaebba341ccb5e8c1168d
bbo <benjamin.boecherer@live.de>
2014-05-13 18:03:27 +0200

    Fehlende Pages hinzugefügt

---
+++ SealingSchoolWPF/Pages/BusinessUnit/Qualification.xaml.cs
@@ -0,0 +1,31 @@
+﻿using SealingSchoolWPF.ViewModel.BusinessUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SealingSchoolWPF.Pages.BusinessUnit
+{
+    /// <summary>
+    /// Interaction logic for Qualification.xaml
+    /// </summary>
+    public partial class Qualification : UserControl
+    {
+        public Qualification()
+        {
+            InitializeComponent();
+            var viewModel = new QualificationListViewModel();
+            qualificationList.DataContext = viewModel;
+        }
+    }
+}
